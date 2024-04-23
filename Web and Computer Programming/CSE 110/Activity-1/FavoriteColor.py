de070e580703d978e8fad18c6d5d336f6aaedcba
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2024-04-23 15:04:58 +0200

    Activity 1
    Write a simple python program that asks a user for their favorite color then displays the color to the console.

---
+++ Web and Computer Programming/CSE 110/Activity-1/FavoriteColor.py
@@ -0,0 +1,24 @@
+""" 
+AUTHOR:  MICHAEL KAZEMBE
+DATE:  23/04/2024
+COURSE:  CSE 110: Introduction to Programming
+INSTRUCTOR:  CHAD OWENS
+ASSIGNMENT:  Activity 1 - Favorite Color
+PURPOSE:  To write a program that asks a user for their favorite color, then allow them to type in their color. Finally, have the program respond to them by displaying the text "Your favorite color is" followed by the color they typed.
+
+Activity Instructions:
+For this activity you will write a program that uses both input (obtaining data from the user via the keyboard) and output (displaying data to the user on the screen).Instructions:
+Write a program that asks a user for their favorite color, then allow them to type in their color. Finally, have the program respond to them by displaying the text "Your favorite color is" followed by the color they typed.
+
+Example Output:
+Enter your favorite color: Blue
+Your favorite color is Blue
+
+"""
+
+# Get the user's favorite color
+color = input("Enter your favorite color: ")
+# Display the user's favorite color
+print("Your favorite color is")
+# Display the color the user typed
+print(color)
