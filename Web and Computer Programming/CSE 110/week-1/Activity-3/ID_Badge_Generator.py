e1aaa73aab9960603e929289cf60b9fae87f59b0
MichaelKazembe <122834780+MichaelKazembe@users.noreply.github.com>
2024-04-25 23:54:40 +0200

    Stretch Challenge
    String formatting to align text

--- Web and Computer Programming/CSE 110/week-1/Activity-3/ID_Badge_Generator.py
+++ Web and Computer Programming/CSE 110/week-1/Activity-3/ID_Badge_Generator.py
@@ -11,27 +11,41 @@ the ID card with the user's information.
 
 print("Please enter the following information: \n")
 # Prompt User For Their First Name
-first_name = input("Enter Your First Name: ")
+first_name = input("Enter Your First Name: ").upper()
 # Prompt User For Their Last Name
-last_name = input("Enter Your Last Name: ")
+last_name = input("Enter Your Last Name: ").capitalize()
 # Prompt User For Their Email
-email = input("Enter Your email: ")
+email = input("Enter Your email: ").lower()
 # Prompt User For Their Phone Number
 phone = input("Enter Your phone number: ")
 # Prompt User For Their Job Title
-job_title = input("Enter Your job title: ")
+job_title = input("Enter Your job title: ").title()
 # Prompt User For Their ID Number
 ID_number = input("Enter Your ID number: ")
 
+# Prompt User For Their Hair Color
+hair = input("Enter Your Hair Color: ")
+# Prompt User For Their Eye Color
+eye = input("Enter Your Eye Color: ").capitalize()
+# Prompt User For Month They Started Working
+month = input("Enter The Month You Started Working: ")
+# Prompt User Whether They Completed Advanced Training
+training = input("Did You Complete Advanced Training? (Yes/No): ")
 
-# Print Personal Information
+# Print Personal Information With Stretch Challenge
+print("\nThe ID Card is:")
+print("-" * 42)
+print(f"{last_name}, {first_name}")
+print(job_title)
+print(f"ID: {ID_number}")
 print("\n")
-print("The ID Card is:")
-print("------------------------------------------")
-print(last_name.upper() + ", " + first_name.capitalize())
-print(job_title.title())
-print("ID: " + ID_number)
-print("\n")
-print(email.lower())
+print(email)
 print(phone)
-print("------------------------------------------")
+print("\n")
+# Stretch Challenge: formatting the string to the right to align the text
+print(f"Hair: {hair:<15} Eyes: {eye.capitalize()}") # string Formatting 15 spaces to the right
+print(f"Month: {month:<14} Training: {training.capitalize()}") # string Formatting 14 spaces to the right
+print("-" * 42)
+
+# End Of Program
+
--- Web and Computer Programming/CSE 110/week-1/Activity-3/README.md
+++ Web and Computer Programming/CSE 110/week-1/Activity-3/README.md
@@ -44,11 +44,7 @@ In addition to the spacing and punctuation shown above, you must implement the f
 - The job title should be displayed so that the first letter is capitalized.
 - The email address should be displayed in all lowercase.
 
-
-
 ---
-
-
 
 An example of the program running is shown:
 
@@ -68,8 +64,6 @@ An example of the program running is shown:
 
 ---
 
-
-
 `The ID Card is:`
 
 `----------------------------------------------`
@@ -85,5 +79,38 @@ janedoe@email.com
 (208) 555-1234
 
 `----------------------------------------------`
+
+
+#### Stretch Challenge
+
+---
+
+Most team activities will also contain stretch challenges, which are not specifically required by the assignment, but are a great way to dive deeper into the material. They can be more difficult and may require you to find solutions that weren't directly covered in the preparation material.
+
+The stretch challenges for this activity are:
+
+1. Add hair color and eye color and put them both on the same line in the display.
+2. Add a field for the name of the month they started and also a yes/no field for whether they have completed advanced training. Put these both on the same line in the display.
+3. For the two lines that you just added, make it so that the second columns align, regardless of how many letters are in the responses.
+   To complete this one, you may need to search the internet for something like, "python spacing format" or something similar to see if you get any ideas.
+
+At the end of the stretch challenge, your output should look something like the following:
+
+```bash
+
+The ID Card is:
+----------------------------------------
+DOE, Jane
+Chief Software Architect
+ID: 83-23821
+
+janedoe@email.com
+(208) 555-1234
+
+Hair: Brown           Eyes: Blue
+Month: September      Training: Yes
+----------------------------------------
+```
+
 
 ---
